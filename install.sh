ab94d2cd0ebe81ed245cb7d0faa78c72e5fa8b6d
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-11-23 11:36:56 -0600

    Added install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+git submodule init
+git submodule update
+
+echo source ~/.vim/vim-for-rails.vim >> ~/.vimrc
+echo source ~/.vim/gvim-for-rails.vim >> ~/.gvimrc
+echo source ~/.gh-dotfiles/profile >> ~/.profile
+echo source ~/.gh-dotfiles/profile >> ~/.bashrc
